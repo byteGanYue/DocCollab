caddeb4c1192cb1b349ef090fed2c80895096e0d
Giao <Giao@163.com>
2025-06-15 15:07:03 +0800

    feat: :sparkles: 配置路径别名

--- packages/doc-web/src/main.jsx
+++ packages/doc-web/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
-
+import { router } from '@/router';
+console.log(router, 'rout');
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />,
 );
