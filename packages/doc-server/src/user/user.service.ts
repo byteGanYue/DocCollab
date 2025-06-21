a8df597b4610d555b71f8ca1c8063c9220dcba68
Ni0duann <2014567981@qq.com>
2025-06-21 13:43:54 +0800

    feat: :sparkles: 添加用户公开状态功能，更新用户模型和服务逻辑

--- packages/doc-server/src/user/user.service.ts
+++ packages/doc-server/src/user/user.service.ts
@@ -50,7 +50,13 @@ export class UserService {
     }
   }
 
-  // 用户登陆逻辑
+  /**
+   * 登录功能
+   *
+   * @param loginUserDto 登录用户数据传输对象
+   * @returns 登录结果，包含状态码、用户名和登录成功信息
+   * @throws 如果用户不存在或密码错误，则抛出错误
+   */
   async login(loginUserDto: LoginUserDto) {
     const user = await this.userModel.findOne({ email: loginUserDto.email });
     if (!user) {
@@ -71,6 +77,25 @@ export class UserService {
   }
 
   /**
+   * 修改用户公开状态
+   *
+   * @param id 用户ID
+
+   * @returns 返回修改后的用户对象
+   */
+  async isPublic(email: string) {
+    const user = await this.userModel.findOne({ _email: email });
+
+    if (!user) {
+      throw new Error(`User with email ${email} not found`);
+    }
+    user.isPublic = !user.isPublic;
+
+    await user.save();
+    return user;
+  }
+
+  /**
    * 异步查询所有用户信息
    *
    * @returns 返回包含用户id、用户名、邮箱、创建时间和更新时间的对象数组
@@ -85,6 +110,12 @@ export class UserService {
     });
   }
 
+  /**
+   * 根据给定的ID查找单个用户信息
+   *
+   * @param id 用户ID
+   * @returns 返回一个包含用户信息的Promise对象，如果未找到则返回null
+   */
   async findOne(id: string) {
     return this.userModel.findOne({ _id: id }, { __v: 0 }).select({
       id: 1,
