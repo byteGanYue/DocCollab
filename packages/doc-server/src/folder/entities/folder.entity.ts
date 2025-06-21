14d67f02fc2484c106562a65bd5f2f93235c1ed1
Ni0duann <2014567981@qq.com>
2025-06-21 18:03:37 +0800

    feat: :sparkles: 添加文件夹实体和模式的属性定义，增强文件夹功能

--- packages/doc-server/src/folder/entities/folder.entity.ts
+++ packages/doc-server/src/folder/entities/folder.entity.ts
@@ -1,1 +1,11 @@
-export class Folder {}
+export class Folder {
+  folderId: string;
+  userId: string; // 拥有者ID
+  folderName: string;
+  parentFolderId: string | null; // 父文件夹ID (null 表示根目录)
+  childrenDocumentIds: string[]; // 所有子文档ID
+  childrenFolderIds: string[]; // 所有子文件夹ID
+  depth: number; // 文件夹层级
+  createTime: Date;
+  updateTime: Date;
+}
